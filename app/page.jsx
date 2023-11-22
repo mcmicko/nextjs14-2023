f64ff3a2b49cd108c7513dabd5b835163e38e5c3
mcmicko <mcmzrc@gmail.com>
2023-11-22 22:15:40 +0100

    Stylize image in Nav comp

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import Feed from '@components/Feed';
 
 const Home = () => {
   return (
     <section className='w-full flex-center flex-col'>
       <h1 className='head_text text-center'>
         Discover & Share <br className='max-md:hidden' />
-        <span>AI-Powered Prompts</span>
+        <span className='orange_gradient text-center'>AI-Powered Prompts</span>
       </h1>
       <p className='desc text-center'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quis quod
         ab a nam autem minima aut sint accusamus molestias.
       </p>
 
-      {/**feed */}
+      <Feed />
     </section>
   );
 };
