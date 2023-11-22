ce07d6eaff03c49ceb78e7ccc6905e3ddcacc93e
mcmicko <mcmzrc@gmail.com>
2023-11-22 21:53:05 +0100

    Init Component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        Discover & Share <br className='max-md:hidden' />
+        <span>AI-Powered Prompts</span>
+      </h1>
+      <p className='desc text-center'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quis quod
+        ab a nam autem minima aut sint accusamus molestias.
+      </p>
+
+      {/**feed */}
+    </section>
+  );
 };
 
 export default Home;
