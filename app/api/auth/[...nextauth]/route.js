b1da66d170e74422e592f831481cbc89375125ac
mcmicko <mcmzrc@gmail.com>
2023-12-04 13:51:03 +0100

    Connection with mongoDB

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -8,12 +11,39 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
 
-  async session({ profile }) {
+    session.user.id = sessionUser._id.toString();
+
+    return session;
+  },
+
+  async signIn({ profile }) {
     try {
       // serverless -> Lambda -> dynamodb
-    } catch (error) {}
+
+      await connectToDB();
+
+      //check if a user alrady exists
+      const userExists = await User.findOne({ email: profile.email });
+
+      //if not, create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(' ', '').toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 
