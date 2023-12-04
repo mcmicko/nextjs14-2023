ccdfc26b0bd324cc83c990ccc3da4685df83b189
mcmicko <mcmzrc@gmail.com>
2023-12-04 15:15:54 +0100

    Connection DB session on Nav & finish OAuth wit google account

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,18 +5,19 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     };
 
-    setProviders();
+    setUpProviders();
   }, []);
   return (
     <div className='flex-between w-full mb-16 pt-3'>
@@ -31,9 +32,11 @@ const Nav = () => {
         <p className='logo_text'>Promptopia</p>
       </Link>
 
+      {alert(providers)}
+
       {/* Desktop navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href='/create-prompt' className='black_btn'>
               Create Post
@@ -71,7 +74,7 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
             <Image
               src='/assets/images/logo.svg'
