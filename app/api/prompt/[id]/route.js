2d6939a76e454ddae0989d37bf3b801f0376f673
mcmicko <mcmzrc@gmail.com>
2023-12-11 21:14:06 +0100

    Delete posts

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -43,10 +43,11 @@ export const DELETE = async (request, { params }) => {
   try {
     await connectToDB();
 
+    // Find the prompt by ID and remove it
     await Prompt.findByIdAndRemove(params.id);
 
     return new Response('Prompt deleted successfully', { status: 200 });
   } catch (error) {
-    return new Response('Failed to delete prompt', { status: 500 });
+    return new Response('Error deleting prompt', { status: 500 });
   }
 };
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,6 @@
 'use client';
-import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 import Profile from '@components/Profile';
@@ -8,6 +8,7 @@ import Profile from '@components/Profile';
 const MyProfile = () => {
   const router = useRouter();
   const { data: session } = useSession();
+
   const [myPosts, setMyPosts] = useState([]);
 
   useEffect(() => {
@@ -19,13 +20,27 @@ const MyProfile = () => {
     };
 
     if (session?.user.id) fetchPosts();
-  }, []);
+  }, [session?.user.id]);
 
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async (post) => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm('Are you sure you want to delete this prompt');
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, { method: 'DELETE' });
+
+        const filteredPosts = myPosts.filter((e) => e._id !== post._id);
+
+        setMyPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return (
     <Profile
       name='My'
